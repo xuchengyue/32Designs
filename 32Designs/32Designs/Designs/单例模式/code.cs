5d6eb0a35311819f507723085a7c9253c2fe1099
ETOCRM\chris.xu <chris.xu@etocrm.com>
2018-08-08 18:58:16 +0800

    submit

---
+++ 32Designs/32Designs/Designs/单例模式/code.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _32Designs.Designs.单例模式
+{
+    public class code
+    {
+        public static LogClass log;
+        public static readonly object lockObj;
+
+        // 饿汉式单例 利用静态构造函数来实现
+        static code()
+        {
+            log = new LogClass();
+        }
+
+        // 懒汉式单例
+        public static LogClass LogInstance
+        {
+            get
+            {
+                if (log == null)
+                {
+                    lock (lockObj)
+                    {
+                        if (log == null)
+                        {
+                            log = new LogClass();
+                        }
+                    }
+                }
+                return log;
+            }
+        }
+
+        // 内部类静态构造实现
+        public static LogClass LogInstance2
+        {
+            get
+            {
+                return Inside.obj;
+            }
+        }
+
+        public class Inside
+        {
+            static Inside() { }
+            internal static LogClass obj = new LogClass();
+        }
+    }
+
+    public class LogClass
+    {
+        //记录日志xxxxxx
+    }
+}
